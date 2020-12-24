378af189bba944c7867663cde7e9e45e32fd2551
dswook <leewook94@naver.com>
2020-12-24 17:01:01 +0900

    Commit bayesian code

---
+++ src/riiid/data/train_dataset.py
@@ -0,0 +1,54 @@
+import gc
+from typing import List, Tuple
+
+import pandas as pd
+
+from data.questions import questions_df
+from data.dataset import train_df
+
+
+def train_dataset() -> Tuple[List, List]:
+    trains = []
+    valids = []
+    num = 1
+
+    for i in range(num):
+        train_df_clf = train_df.sample(n=1200*10000)
+        print('sample end')
+
+        users = train_df_clf['user_id'].drop_duplicates()
+
+        users = users.sample(frac=0.08)
+        users_df = pd.DataFrame()
+        users_df['user_id'] = users.values
+
+        valid_df_newuser = pd.merge(train_df_clf, users_df,
+                                    on=['user_id'], how='inner',
+                                    right_index=True)
+        del users_df
+        gc.collect()
+        train_df_clf.drop(valid_df_newuser.index, inplace=True)
+
+        print('train_df_clf, question_df merge')
+
+        train_df_clf = pd.merge(train_df_clf, questions_df, on='content_id',
+                                how='left', right_index=True)
+
+        valid_df_newuser = pd.merge(valid_df_newuser, questions_df,
+                                    on='content_id', how='left',
+                                    right_index=True)
+
+        valid_df = train_df_clf.sample(frac=0.1)
+        train_df_clf.drop(valid_df.index, inplace=True)
+
+        valid_df = valid_df.append(valid_df_newuser)
+        del users
+        del valid_df_newuser
+        gc.collect()
+
+        trains.append(train_df_clf)
+        valids.append(valid_df)
+        print('train_df_clf length:', len(train_df_clf))
+        print('valid_df length：', len(valid_df))
+
+    return trains, valids
--- src/riiid/train.py
+++ src/riiid/train.py
@@ -1,30 +1,10 @@
 from optim.bayesian_optim import lgb_bayes
-from sklearn.metrics import roc_auc_score
-
-from model.pipeline_model import pipeline_lgb
-from cleaning.dataset import train, target
-from cleaning.dataset import X_train, X_valid, y_train, y_valid
 
 if __name__ == "__main__":
 
     bayesian_params = {
-        'learning_rate': (0.01, 0.5),
-        'max_depth': (1, 30),
-        'num_leaves': (10, 200),
-        'colsample_bytree': (0.5, 1.0),
-        'subsample': (0.1, 1.0)
+        'num_leaves': (300, 1000),
+        'max_bin': (10, 400),
+        'learning_rate': (0.01, 0.1),
     }
-    lgb_bo = lgb_bayes(train, target, bayesian_params)
-
-    params = {
-        "learning_rate": max(min(lgb_bo['learning_rate'], 1), 0),
-        'num_leaves': int(round(lgb_bo['num_leaves'])),
-        'max_depth':  int(round(lgb_bo['max_depth'])),
-        'colsample_bytree': max(min(lgb_bo['colsample_bytree'], 1), 0),
-        'subsample': max(min(lgb_bo['subsample'], 1), 0)
-    }
-
-    lgb_model = pipeline_lgb(train, params)
-    lgb_model.fit(X_train, y_train)
-    lgb_pred = lgb_model.predict_proba(X_valid)[:, 1]
-    print(f'ROC_AUC: {roc_auc_score(y_valid, lgb_pred):.4f}')
+    lgb_bo = lgb_bayes(bayesian_params)
